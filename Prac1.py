ef3285f45409c0152b9a2bb07e95fae8f315b5c9
ZubayrIsmail <zubayrismail@gmail.com>
2019-07-29 00:30:14 +0200

    Toggle Led

--- Prac1.py
+++ Prac1.py
@@ -26,9 +26,17 @@ GPIO.setup(led3, GPIO.OUT)
 GPIO.setup(switch_1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(switch_2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
+global onoff
+onoff = 0
 # function definition: threaded callback
 def callback1(channel):
-    print("Switch 1")
+    global onoff
+    if  onoff == 0:
+        GPIO.output(led1, GPIO.HIGH)
+        onoff = 1
+    else:
+        GPIO.output(led1, GPIO.LOW)
+        onoff = 0
 def callback2(channel):
     print("Switch 2")
 
@@ -38,11 +46,9 @@ def main():
     GPIO.add_event_detect(switch_1, GPIO.RISING, callback = callback1,bouncetime=200)
     GPIO.add_event_detect(switch_2, GPIO.RISING, callback = callback2,bouncetime=200)
     while(1):
+        x=1
 
-        GPIO.output(led1,GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(led1,GPIO.LOW)
-        time.sleep(0.5)
+
     print("write your logic here")
 # Under a falling-edge detection, regardless of current execution
 # callback function will be called
