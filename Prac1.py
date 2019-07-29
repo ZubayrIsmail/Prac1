82537a08464e1405a5327d0543a11b607237fb5e
ZubayrIsmail <zubayrismail@gmail.com>
2019-07-29 22:04:43 +0200

    Final binary counter with comments

--- Prac1.py
+++ Prac1.py
@@ -14,6 +14,7 @@ import RPi.GPIO as GPIO
 import time
 import itertools
 
+#Setting pi pinout convention and assigning pin numbers
 GPIO.setmode(GPIO.BOARD)
 switch_1 = 36
 switch_2 = 40
@@ -21,36 +22,38 @@ led1 = 11
 led2 = 13
 led3 = 15
 
+#Setting up led pins as outputs 
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
 
+#Setting push button pins as inputs
 GPIO.setup(switch_1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(switch_2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 GPIO.output(led1, GPIO.LOW)
 GPIO.output(led2, GPIO.LOW)
 GPIO.output(led3, GPIO.LOW)
-
+#creating a global variable
 global count
 count = 0
+#Creating a list containing binary values 
 lst = list(itertools.product([0, 1], repeat=3))
+
 # function definition: threaded callback
+
 def countUp(up):
     global count
     count += 1  
     print(count)
-    #print(lst[1][2])
-    #print(lst)
     print("up pushed")
     if count > 7:
         count = 0
         
-    
+#setting logic to pins     
     GPIO.output(led1,lst[count][0])
     GPIO.output(led2,lst[count][1])
     GPIO.output(led3,lst[count][2])
-    #GPIO.remove_event_detect(switch_1)
     time.sleep(0.3)
     
 
@@ -78,18 +81,14 @@ def countDown(down):
 
 # Logic that you write
 def main():
+    #Creating interrupt detection
     GPIO.add_event_detect(switch_1, GPIO.RISING, callback = countUp,bouncetime=500)
     GPIO.add_event_detect(switch_2, GPIO.RISING, callback = countDown,bouncetime=500)
     
     #GPIO.remove_event_detect(switch_2)
     while(1):
         x=1
-    
 
-    
-    
-        
-    print("write your logic here")
 # Under a falling-edge detection, regardless of current execution
 # callback function will be called
 
